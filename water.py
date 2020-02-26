4314b45c066c1f21f295d8cec5583f7cb50708ec
billcui57 <billcui2001@hotmail.com>
2020-02-26 10:32:06 +0700

    add water.py

---
+++ water.py
@@ -0,0 +1,28 @@
+
+import RPi.GPIO as GPIO
+import datetime
+import time
+
+switch = 4 
+relay = 21 
+state = 1
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setwarnings(False)
+GPIO.setup(switch, GPIO.IN)
+GPIO.setup(relay, GPIO.OUT)
+
+GPIO.output(relay, GPIO.LOW)
+
+start = datetime.time(10, 24,0) #start watering here
+end = datetime.time(10, 24, 5) #water for 5 seconds
+
+while True:
+    timestamp = datetime.datetime.now().time()
+    # if switch is pressed or it is at watering time, then water
+    if (GPIO.input(switch) == 1) or (start <= timestamp <= end):
+        GPIO.output(relay, GPIO.HIGH)
+    # if switch is not pressed (also used to turn off watering after watering time has elapsed)      
+    elif (GPIO.input(switch) == 0):
+        GPIO.output(relay, GPIO.LOW)
+            
